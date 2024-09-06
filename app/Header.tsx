dd6c588d198b935615eb1289a4023a4c52bd66bd
zeroxkitakitsune <kitakitsune@kitakitsune.dev>
2024-09-07 00:20:05 +0300

    sadsasd

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="mb-12 flex justify-between items-start">
       <div>
         <h1 className="text-4xl font-bold text-white mb-2">jsopn</h1>
-        <p className="text-xl text-gray-500">sleep deprived developer</p>
+        <p className="text-xl text-gray-500">sleep deprivasddased developer</p>
       </div>
       <Image
         src="/placeholder.svg?height=80&width=80"
